cc52d36a87b5d7a786306cbcdfd44ade5714b6f5
san <jocksan.brittos@gmail.com>
2022-05-25 06:16:13 -0300

    Initial navigation setup

--- android/app/src/main/java/com/awesometsproject/MainActivity.java
+++ android/app/src/main/java/com/awesometsproject/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends ReactActivity {
     return "AwesomeTSProject";
   }
   @Override
-  protected void onCreate(Bundle saveInstanceState) {
-    supe.onCreate(null);
-  }
+    protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
