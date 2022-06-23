b2a6b9bd669c1fd366b997f1872719af20cc30ed
san <jocksan.brittos@gmail.com>
2022-06-22 21:17:17 -0300

    create server and configuration routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 import useAppearance from './hooks/useAppearance/index';
 import {NavigationContainer} from '@react-navigation/native';
@@ -7,6 +7,11 @@ import {AuthProvider} from './context/Auth';
 
 const App: React.FC = () => {
     const {theme} = useAppearance();
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/test').then(res => console.log(res));
+    }, []);
+
     return (
         <ThemeProvider theme={theme}>
             <NavigationContainer>
