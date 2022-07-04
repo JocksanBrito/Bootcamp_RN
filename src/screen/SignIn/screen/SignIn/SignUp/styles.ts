b2615338cb074ca23c0d1539f0b7d7ce593dc005
san <jocksan.brittos@gmail.com>
2022-07-04 11:46:38 -0300

    SignUp screen

--- src/screen/SignIn/screen/SignIn/SignUp/styles.ts
+++ src/screen/SignIn/screen/SignIn/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import {Pressable} from 'react-native';
 import styled from 'styled-components/native';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import Text from '~/components/Text';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export const Container = styled(SafeAreaView)`
     flex: 1;
